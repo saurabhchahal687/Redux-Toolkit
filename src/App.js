acc5c228a23bf2186a7edcca921e4bfd6d6a83d8
saurabh chahal <saurabh.chahal6879@gamil.com>
2025-08-26 14:11:32 +0530

    redux practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import {increment, reset,  decrement, incrementByAmount} from './features/counter/counterSlice';
+import { useState } from 'react';
 
 function App() {
+  const [amount,setAmount] =useState(0)
+  const count = useSelector((state) =>state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleIncrementClick(){
+    dispatch(increment());
+  }
+
+  function handleDecrementClick(){
+    dispatch(decrement())
+  }
+
+  function handleResetClick(){
+    dispatch(reset())
+  }
+
+  function handleIncAmountClick(){
+    dispatch(incrementByAmount(amount));
+  }
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <button onClick={handleIncrementClick}>+</button>
+      <p>Count:{count}</p>
+      <button onClick={handleDecrementClick}>-</button>
+      <br/>
+      <br/>
+      <button onClick={handleResetClick}>Reset</button>
+      <br/>
+      <input 
+        type='number'
+        value={amount}
+        placeholder='Enter Amount'
+        onChange={(e)=> setAmount(e.target.value)}
+      />
+      <br/>
+      <br/>
+      <button onClick={handleIncAmountClick}>Inc by Amount</button>
     </div>
   );
 }
